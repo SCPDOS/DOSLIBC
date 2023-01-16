daabbbbba8073f6167af9025ecbf1b8169a98179
ybuzoku <ybuzoku@hotmail.co.uk>
2023-01-16 23:49:58 +0000

    Upgraded CRT0 to create a proper char *argv[]. Dynamically allocates memory from OS. Replace eventually with _malloc call

--- CRT0/source/crt0.asm
+++ CRT0/source/crt0.asm
@@ -8,41 +8,39 @@ extern _argv    ;Argument Vector
 extern _argc    ;Argument Count
 extern _env     ;Environment
 
-global start 
+global __start__
 global __main
     section .text
 ;TODO:
-;1) Paragraph align the stack (downwards ofc)
-;2) Clean the BSS
-;3) Get a pointer to the command line. Count the number of times 
-;   a space/tab region appears, to pass as argc in rcx and the 
-;   pointer to start of the command line in rdx.
-;   Make sure to null terminate each string 
-;       (replace first char in space region with 00).
-;
-;4) Call main.
-;5) If eax > 0FFh, set al = 0FFh and 41h/AH=4Ch exit
+;0) Paragraph align the stack
+;1) Clean the BSS
+;2) Process command line to get argc, argv
+;3) Call main.
+;4) If eax > 0FFh, set al = 0FFh and 41h/AH=4Ch exit
 ;Future expansion:
 ;Hook Int 00h to prevent termination in the event of such a bug.
 ; We first print an error message, then pop the RIP value from 
 ; the stack, decode the instruction and go to the next 
 ; instruction after it!
-start:
-;Step 1)
-;BREAKPOINT BREAKPOINT BREAKPOINT BREAKPOINT 
-   ;xchg bx, bx
-;BREAKPOINT BREAKPOINT BREAKPOINT BREAKPOINT 
+__start__:
+;BREAKPOINT BREAKPOINT BREAKPOINT BREAKPOINT
+    ;xchg bx, bx
+;BREAKPOINT BREAKPOINT BREAKPOINT BREAKPOINT
+    cld ;Ensure the direction is good (DOS does this anyway)
+;Step 0)
+;Realign the stack 
     mov rax, rsp
-    shr rax, 5  ;Divide by 16
-    shl rax, 5  
+    shr rax, 4  ;Divide by 16
+    shl rax, 4  
     mov rsp, rax
-;Step 2)
+;Leave rsp alone now
+;Step 1)
     mov rdi, _BSS_START_
     mov rcx, _BSS_END_
     sub rcx, rdi
     xor eax, eax
     rep stosb
-;Step 3)
+;Step 2)
     ;We're gonna be proper and ask DOS to give us the ptr to ENV and CMDLINE
     ;The cmdline is guaranteed to be at ptr + 37 (+36 gives number of chars)
     mov eax, 0x6100 ;New System Service, get environment ptr
@@ -55,35 +53,76 @@ start:
     cmp ecx, eax    ;Is the number of chars bigger than 127?
     cmova ecx, eax  ;If so, replace it with 127
     lea rdi, qword ptr [rdx + 37]   ;Get the ptr to the char array
-    push rdi    ;Push the pointer to the start of the char array
+    mov rsi, rdi    ;Save the ptr to the string array in rsi
     xor edx, edx    ;Clear the argc counter
-    mov al, " " ;Search for space
-scanLp:
-    repne scasb
-    jecxz step4 ;No more chars, exit
+    mov al, " "     ;Search for space
+makeCstrings:
+    repne scasb     ;Search for first space past name
+    jecxz endOfCmdLine ;No more chars, exit and terminate current string
+    mov byte [rdi - 1], 0   ;Null terminate this string
+    inc edx ;One more string processed
     repe scasb  ;Skip all the spaces
-    jecxz step4
-    ;rdi points to the char after the first non-space
-    dec rdi ;Go to the first non-space char
-    mov byte [rdi - 1], 0   ;Replace the last space with a null
-    inc edx ;One more char processed
-    jmp short scanLp 
-step4:
-;Step 4) 
-    mov byte [rdi], 0   ;Store a final terminating null
-    inc edx ;Add one more (if none, for cmdname)
-    mov ecx, edx
-    pop rdx ;Pop the pointer back
-    mov qword [_argc], rcx
-    mov qword [_argv], rdx
+    test ecx, ecx
+    jnz makeCstrings
+    cmp byte [rdi - 1], " "
+    jne endOfCmdLine    ;If the last char was not a space, skip the dec
+    dec edx ;Dec the count to balance the below (as we searched thru spaces)
+endOfCmdLine:
+    inc edx ;Add one more char which we are about to terminate
+    mov byte [rdi], 0   ;Store a final null over terminating 0Dh
+    mov qword [_argc], rdx  ;Save the number of arguments we have
+    mov eax, 0x4800
+    lea rbx, qword [8*rdx + 0x11] ;Get the number of bytes to allocate
+    shr rbx, 4  ;Divide by 16 to get number of paragraphs
+    int 41h
+    jc exitBad
+    mov rbp, rax    ;Store the ptr to the array here
+;argv array must have a qword at _argv[_argc] = 0
+    mov qword [_argv], rbp  ;Save the ptr to the char* array
+
+    mov rdi, rsi    ;Get the start of the command line string into rdi
+    xor edx, edx    ;Zero the offset register
+    xor ecx, ecx
+    dec ecx         ;Get -1 in ecx
+    xor eax, eax    ;Scan for nulls
+    mov ah, " "     ;Prepare for space scanning
+buildArgv:
+    mov qword [rbp + 8*rdx], rdi  ;Save rdi as the ptr to the string
+    repne scasb   ;Scan for the terminating null
+    xchg al, ah 
+    repe scasb    ;Scan through the spaces to char 1 of the string
+    dec rdi     ;Go back to the first char since scasb goes to next char
+    xchg al, ah
+    inc edx
+    cmp rdx, qword [_argc]  ;Are we equal yet?
+    jne buildArgv
+    xor eax, eax
+    mov qword [rbp + 8*rdx], rax    ;Store a ptr to NULL
+    mov rcx, qword [_argc]  ;Get the regs in for the calling convention
+    mov rdx, qword [_argv]  ;Get the regs in for the calling convention
+    mov r8, qword [_env]    ;Get the optional, UNPARSED environment
+    ;We pass a ptr to the environment but it is unparsed as ANSI doesn't 
+    ; request this!
+;Step 3) 
     call main
-;Step 5)
+;Step 4)
+    mov r8, qword [_argv]   ;Get the pointer to free this block
+    mov eax, 0x4900 ;Explicitly try to free
+    int 0x41
+exitCommon:
     mov ecx, 0xFF
     cmp eax, ecx
     cmova eax, ecx 
     or eax, 0x4C00  ;Add the exit code into AH w/o stall
     int 0x41 ;Return to DOS
-
+;If there is an allocation error, exit
+exitBad:
+    lea rdx, badMemStr
+    mov eax, 0x0900
+    int 0x41
+    mov eax, 0xFF
+    jmp short exitCommon
+badMemStr db "CRT: Not enough memory",0Ah,0Dh,"$"
 ;GCC provides a call to this main constructor. Since we 
 ; setup everything in assembly in CRT0, we don't need this.
 __main:
